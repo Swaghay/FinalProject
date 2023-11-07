b563d6feb4df3d570d8884d9d972d017d9b6e4e0
Tyler0229 <150181772+Tyler0229@users.noreply.github.com>
2023-11-07 12:57:22 -0600

    hello

--- lib/Ast.ml
+++ lib/Ast.ml
@@ -12,3 +12,5 @@ let apple_property (h: int) (s: int) =
   apple apple h s = apple apple s h
 
 Let(apple_property, [(h*int),(s*int)], Application(apple (Application Var h,Var s)), Application(apple (apple, s), h))
+
+...
