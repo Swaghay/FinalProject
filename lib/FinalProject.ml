3616133d40bf8754f01fafd4747c6b340fd8a861
Tyler0229 <150181772+Tyler0229@users.noreply.github.com>
2023-11-08 15:30:48 -0600

    string_of_pattern

--- lib/FinalProject.ml
+++ lib/FinalProject.ml
@@ -2,3 +2,13 @@ let parse (s : string) : string list =
   let lexbuf = Lexing.from_string s in
   let ast = Parser.main Lexer.token lexbuf in
      ast
+
+(* let rec string_of_pattern (p : pattern) : string =
+match p with
+| Constructor (name, []) -> name
+| Constructor (name, patterns)
+-> name ^ " (" ^
+(String.concat ", “
+(List.map string_of_pattern patterns)
+) ^ ")"
+| Variable (name, type_name) -> name ^ " : " ^ type_name *)
