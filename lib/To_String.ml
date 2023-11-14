19f739a2923a9ffe2d6f7a118868b43d4450419a
swaghay <upabhay@gmail.com>
2023-11-14 13:25:26 -0600

    a

--- lib/To_String.ml
+++ lib/To_String.ml
@@ -5,7 +5,11 @@ let rec string_of_expression (p : expression) : string =
 (String.concat " " (List.map string_of_expression args)) ^ ")"
   |Name name -> name
 
+let string_of_typedNameList (l: typedName list) : string =
+  match l with
+  | [] -> ""
+  | (x, y)::t -> "( " ^ x ^ " : " ^ " ) " ^ (string_of_typedNameList t)
 
-in string_of_decl (p : decl) : string =
-
+let string_of_decl (Let (name,args,exp1,exp2)) : string =
+"Let " ^ name ^ " " ^ (string_of_typedNameList args) ^ "\n= (" ^ (string_of_expression exp1) ^  " = " ^ (string_of_expression exp2) ^ ")\n"
 
