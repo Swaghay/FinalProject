7b8a9cd37d631e6241859380875ad4d71007c2f9
swaghay <upabhay@gmail.com>
2023-11-14 12:18:32 -0600

    abhay

--- lib/To_String.ml
+++ lib/To_String.ml
@@ -1,6 +1,12 @@
 let rec string_of_expression (p : expression) : string =
   match p with
-  | Application (Name name1, expression list name2) -> "(" ^ name1 ^ " " ^ name2 ^ ")"
+  | Application (name, args)
+-> string_of_expression (name) ^ " (" ^
+(String.concat " "
+(List.map string_of_expression args)
+) ^ ")"
+  |Name name -> name
+
   (* | Application (Application app, Name name)
   | Application (Name name, Application app) *)
 
