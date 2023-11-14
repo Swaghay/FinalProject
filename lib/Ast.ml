8994790b42a5aca60ceb57839a7879ebdd3d4f21
swaghay <upabhay@gmail.com>
2023-11-14 10:06:24 -0600

    a

--- lib/Ast.ml
+++ lib/Ast.ml
@@ -15,7 +15,11 @@ type decl =
 let apple_property (h: int) (s: int) =
   apple (apple h s) s = apple (apple s h) s
 
-
 (apple ((apple h) s)) s
 Application (Application (Func "apple", Application (Application (Func "apple", Func "h"), Func "s")), Func "s")
+
+(h: int) (s: int)
+[Arguments ("h", "int"), Arguments ("s", "int")]
+
+
 *)
