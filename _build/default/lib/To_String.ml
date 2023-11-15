afb04814f1cfc65f3101ad69e5cccba2dc7f00f5
swaghay <upabhay@gmail.com>
2023-11-15 11:33:38 -0600

    m

--- _build/default/lib/To_String.ml
+++ _build/default/lib/To_String.ml
@@ -12,6 +12,6 @@ let rec string_of_typedNameList (l: typedName list) : string =
   | [] -> ""
   | (Arguments (x, y))::t -> "( " ^ x ^ " : " ^ y ^ " ) " ^ (string_of_typedNameList t)
 
-let string_of_decl (Let (name,args,exp1,exp2)) : string =
+let string_of_declaration (Let (name,args,exp1,exp2)) : string =
 "Let " ^ name ^ " " ^ (string_of_typedNameList args) ^ "\n= (" ^ (string_of_expression exp1) ^  " = " ^ (string_of_expression exp2) ^ ")\n"
 
